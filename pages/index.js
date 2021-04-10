5d83c030a25dbed18d1f3396faf860db123d8488
Lincoln Gachagua <blazeboy.wachira@gmail.com>
2021-04-10 09:30:58 +0200

    Added 1920 support

--- pages/index.js
+++ pages/index.js
@@ -33,8 +33,12 @@ export default function Home() {
 
       <div className={styles.reposearch}>
         <div className={styles.repoinput}>
-          <FontAwesomeIcon icon={faSearch} className={styles.font}></FontAwesomeIcon>
-          <input type="text" placeholder="Eg. facebook/react" className={styles.buttontext} />
+
+          <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.font}>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.8973 11H11.1462L10.88 10.73C12.0209 9.33002 12.6103 7.42001 12.2871 5.39002C11.8402 2.61002 9.63465 0.390015 6.97273 0.0500152C2.95133 -0.469985 -0.433105 3.09002 0.0612515 7.32002C0.384484 10.12 2.495 12.44 5.13791 12.91C7.0678 13.25 8.88361 12.63 10.2146 11.43L10.4713 11.71V12.5L14.5117 16.75C14.9014 17.16 15.5384 17.16 15.9282 16.75C16.318 16.34 16.318 15.67 15.9282 15.26L11.8973 11ZM6.19317 11C3.82596 11 1.91509 8.99002 1.91509 6.50002C1.91509 4.01002 3.82596 2.00002 6.19317 2.00002C8.56037 2.00002 10.4713 4.01002 10.4713 6.50002C10.4713 8.99002 8.56037 11 6.19317 11Z" fill="#29335C" />
+          </svg>
+
+          <input type="text" placeholder="Eg. facebook/react" className={styles.repotext} />
         </div>
 
         <div className={styles.buttoncommit}>
@@ -50,8 +54,6 @@ export default function Home() {
           <button type="button" placeholder="Eg. facebook/react" className={styles.suggestionitem}>microsoft/vscode</button>
           <button type="button" placeholder="Eg. facebook/react" className={styles.suggestionitem}>jezen/is-thirteen</button>
           <button type="button" placeholder="Eg. facebook/react" className={styles.suggestionitem}>benawad/dogehouse</button>
-
-
         </div>
 
       </div>
